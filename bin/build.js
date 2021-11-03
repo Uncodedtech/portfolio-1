393ad975350c59d46b3b7aa74c3529d744c490d4
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-11-02 21:47:09 -0500

    Revert "Add api to build script."
    This reverts commit ab0b1bef47d1f7682bfdd19d6354588208eff119.

--- bin/build.js
+++ bin/build.js
@@ -5,9 +5,7 @@ const { exec } = require('child_process');
 
 const RESERVED_DIRS = ['_next', 'static'];
 const EXCLUDED_ROUTES = ['200', '404'];
-
 const BUILD_DIR = path.resolve(process.cwd(), 'build');
-const API_DIR = path.resolve(process.cwd(), 'api');
 
 /**
  * Executes a command, returning promisified stdout.
@@ -15,63 +13,32 @@ const API_DIR = path.resolve(process.cwd(), 'api');
 const run = async command => new Promise(res => exec(command, res));
 
 /**
- * Checks whether path is a folder.
+ * De-dups and optimizes HTML output.
  */
-const isFolder = filePath => fs.lstatSync(filePath).isDirectory();
-
-/**
- * Crawls through directories via a matcher, returning a list of matched files.
- */
-const crawl = (dir, matcher, crawled = [], root = true) => {
+const optimizeHTML = (dir = '', routes = []) => {
   // Scan directory for file contents
-  const files = fs.readdirSync(dir);
+  const files = fs.readdirSync(path.resolve(BUILD_DIR, dir));
 
   // Sort through files
-  files.forEach(file => {
-    const filePath = path.resolve(dir, file);
-    const fileName = file.replace(/\.\w+/g, '');
-    const localPath = `${dir}/${fileName}`;
+  files.forEach(entry => {
+    // Delete duplicate HTML
+    const fileName = entry.replace(/\.\w+/g, '');
+    const localPath = dir ? `${dir}/${fileName}` : fileName;
 
-    // Add matching routes to coverage
-    if (matcher.test(file) && !EXCLUDED_ROUTES.includes(fileName)) {
+    if (entry.endsWith('.html') && files.includes(fileName)) {
+      fs.unlinkSync(path.resolve(BUILD_DIR, dir, entry));
+    } else if (entry.endsWith('.html') && !EXCLUDED_ROUTES.includes(localPath)) {
       const pathName = localPath.replace(/(^index|\/index)$/, '');
-      return crawled.push(`/${pathName}`);
+      routes.push(`/${pathName}`);
     }
 
     // Recursively optimize HTML in subfolders
-    if (isFolder(filePath) && !RESERVED_DIRS.includes(file)) {
-      return crawl(localPath, matcher, crawled, false);
+    if (!entry.includes('.') && !RESERVED_DIRS.includes(entry)) {
+      optimizeHTML(localPath, routes);
     }
   });
 
-  // Cleanup paths
-  if (root) {
-    return crawled.map(route => route.replace(`/${dir}`, '/').replace(/\/{2,}/g, '/'));
-  }
-};
-
-/**
- * De-dups HTML output.
- */
-const cleanHTML = dir => {
-  // Scan directory for file contents
-  const files = fs.readdirSync(dir);
-
-  // Sort through files
-  files.forEach(file => {
-    const filePath = path.resolve(dir, file);
-    const fileName = file.replace(/\.\w+/g, '');
-
-    // Delete duplicate HTML
-    if (filePath.endsWith('.html') && files.includes(fileName)) {
-      return fs.unlinkSync(filePath);
-    }
-
-    // Recursively clean HTML in subfolders
-    if (isFolder(filePath) && !RESERVED_DIRS.includes(file)) {
-      return cleanHTML(filePath);
-    }
-  });
+  return routes;
 };
 
 (async () => {
@@ -82,16 +49,10 @@ const cleanHTML = dir => {
   await run('next build');
   await run('next export -o build/');
 
-  // Build API
-  await run('rimraf api && rollup -c');
-
   // Pre-render & optimize
   await run('react-snap');
-  cleanHTML(BUILD_DIR);
+  const routes = optimizeHTML();
 
-  // Display built routes
-  const apiRoutes = crawl(API_DIR, /\.js$/).map(route => `/api${route}`);
-  const buildRoutes = crawl(BUILD_DIR, /\.html$/);
-
-  console.info(`\nBuilt routes:\n\n  ${[...apiRoutes, ...buildRoutes].join('\n  ')}\n`);
+  // Display routes
+  console.info(`\nBuilt routes:\n\n  ${routes.join('\n  ')}\n`);
 })();
